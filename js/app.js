f606a4e021ec3b8063697f62fa0296a34a324f0b
jfuller2 <john.fuller40@gmail.com>
2015-11-10 09:51:42 -0500

    Added hadouken sound

--- js/app.js
+++ js/app.js
@@ -9,9 +9,10 @@ $(document).ready(function() {
 	})
 	.mousedown(function() {
 		// Play hadouken sound
+		playhadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show().animate( 
+		$('.hadouken').finish().show().animate( 
 			{ 'left': '1020px' },
 			500,
 		function() {
@@ -30,6 +31,16 @@ $(document).ready(function() {
 
 });
 
+function playhadouken() {
+	$('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
+}
+
+
+
 // 1. Explain why this is chaining. Does it have to do with the lack of the semi-colon?
 // 2. Explain in detail what a callback function is and exactly what it does
 // 3. Walk me thru the hadouken function starting at line 14
+// 4. Help me with the syntax - brackets, braces, etc....what is that telling the browser?
+// 5. Walk me thru how to think about JS and how to implement it
