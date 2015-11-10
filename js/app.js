f058349a6a503a5df649ffb63ac4e34fc982295e
jfuller2 <john.fuller40@gmail.com>
2015-11-09 20:55:28 -0500

    Hadouken animation

--- js/app.js
+++ js/app.js
@@ -8,18 +8,28 @@ $(document).ready(function() {
 		$('.ryu-ready').hide();
 	})
 	.mousedown(function() {
+		// Play hadouken sound
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
-		// Play hadouken sound
+		$('.hadouken').show().animate( 
+			{ 'left': '1020px' },
+			500,
+		function() {
+			$(this).hide();
+			$(this).css('left', '520px');
+			}
+		);
 		// Show hadouken and animake it to the right of the screen
 	})
 	.mouseup(function() {
 		$('.ryu-ready').show();
 		$('.ryu-throwing').hide();
 		// ryu goes back to its ready position
-	});
+	})
+	
 
 });
 
 // 1. Explain why this is chaining. Does it have to do with the lack of the semi-colon?
+// 2. Explain in detail what a callback function is and exactly what it does
+// 3. Walk me thru the hadouken function starting at line 14
